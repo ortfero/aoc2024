1c2c3c01dd61243a428ae0ed3ff1b002703d225b
Andrei Ilin <ortfero@gmail.com>
2024-12-02 16:48:29 +0200

    Day 01

---
+++ day01/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"sort"
+)
+
+func main() {
+	input, err := os.Open("day01/input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	scanner := bufio.NewScanner(input)
+	lefts := make([]uint, 0, 1001)
+	rights := make([]uint, 0, 1001)
+	var l, r uint
+	for scanner.Scan() {
+		line := scanner.Text()
+		if n, err := fmt.Sscan(line, &l, &r); err != nil || n != 2 {
+			log.Fatal(err)
+		}
+		lefts = append(lefts, l)
+		rights = append(rights, r)
+	}
+	if scanner.Err() != nil {
+		log.Fatal(scanner.Err())
+	}
+	sort.Slice(lefts, func(i, j int) bool { return lefts[i] < lefts[j] })
+	sort.Slice(rights, func(i, j int) bool { return rights[i] < rights[j] })
+	var sum uint
+	for i := 0; i < len(lefts); i++ {
+		l = lefts[i]
+		r = rights[i]
+		if l >= r {
+			sum += l - r
+		} else {
+			sum += r - l
+		}
+	}
+	fmt.Println(sum)
+}
